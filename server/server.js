5b6efc700381119815af1c8580cc5b1f9cecc89d
Ravi Musalin <ravimusalin@Ravis-Air.lan>
2021-04-04 19:07:53 -0400

    First commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+// use body parser to get data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use API routes from the api folder
+const apis = require("./api");
+app.use("/api", apis);
+
+// Connect to Mongo
+ mongoose.connect(process.env.DB, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+ }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use body parser to get data from POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use API routes from the api folder
+const apis = require("./api");
+app.use("/api", apis);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
